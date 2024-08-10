ba905d030d747708fbd3b04a181fdbe7753e553e
AdarshaCoding <adarshapc4467@gmail.com>
2024-08-10 18:57:14 +0530

    Node - modules, require

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+// const calc = require("./calc.js");
+const { add, sub, name1 } = require("./calc");
+
 var name = "Namaste NodeJS";
 var a = 10;
 var b = 20;
@@ -5,3 +8,10 @@ var b = 20;
 console.log(name);
 console.log(a + b);
 console.log(global == globalThis);
+
+// calc.add(a, b);
+// calc.sub(a, b);
+
+add(a, b);
+sub(a, b);
+console.log(name1);
---
+++ calc.js
@@ -0,0 +1,14 @@
+function add(a, b) {
+  const result = a + b;
+  console.log(result);
+}
+
+function sub(a, b) {
+  const result = a - b;
+  console.log(result);
+}
+
+var name1 = "Adarsha";
+
+module.exports = { add, sub, name1 };
+// module.exports = { add: add, sub: sub };
